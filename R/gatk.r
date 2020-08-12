7598bca4bdd577d937f85b9ea5b65b5f87499c4a
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-08-13 10:20:46 +1200

    Picard commands in GATK should use GATK style of parameters (-I instead of I=...). Also, no nthreads parameter?

--- R/gatk.r
+++ R/gatk.r
@@ -1,16 +1,16 @@
 
 
 
-gatk_mark_duplicates = function(input, output, nthreads=4, remake=FALSE){
+gatk_mark_duplicates = function(input, output, remake=FALSE){
     if(!remake && file.exists(output))
         return(invisible())
 
     log = paste0(output, ".txt")
     args = c(
         "MarkDuplicates",
-        paste0("I=", input),
-        paste0("O=", output),
-        paste0("M=", log)
+        "-I", input,
+        "-O", output,
+        "M", log
         )
 
     command = getOption("phyloRNA.gatk")
@@ -18,16 +18,18 @@ gatk_mark_duplicates = function(input, output, nthreads=4, remake=FALSE){
     }
 
 
-gatk_sort = function(input, output, nthreads=4, remake=FALSE){
+gatk_sort = function(input, output, remake=FALSE){
     if(!remake && file.exists(output))
         return(invisible())
 
     args = c(
         "SortSam",
-        paste0("I=", input),
-        paste0("O=", output),
-        paste0("SORT_ORDER=", "coordinate")
+        "-I", input,
+        "-O", output
+        "-SORT_ORDER", "coordinate"
         )
+
     command = getOption("phyloRNA.gatk")
     systemE(command=command, args=args)
     }
+
