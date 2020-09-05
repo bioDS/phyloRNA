8bb3ac38e72ea73155a5ca301a74474e954896d4
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-09-05 17:39:51 +1200

    Added Python script and its R binding to modify bam tags with regex

---
+++ R/bamtagregex.r
@@ -0,0 +1,39 @@
+
+
+
+#' Change sam/bam tag values
+#'
+#' Change tag value in sam/bam file using regular expression.
+#'
+#' This code is a thin wrapper over the Python script `bamtagregex.py` found
+#' in the package's `inst` folder.
+#'
+#' The `bamtagregex.py` uses the amazing `pysam` package to manipulate the sam/bam files.
+#' The Python's `pysam` package is powered by the `htslib` library. the same library
+#' that powers `samtools`.
+#' In R, there is a similar package to `pysam`, the `Rsamtools` found in bioconductor. But this
+#' library reads the whole file at once (instead of using memory efficient iterators), which
+#' makes working with a large bam files less than ideal.
+#'
+#' @template io
+#' @param tag a tag to modify
+#' @param pattern a regex pattern
+#' @param replace a string to replace matched pattern
+#'
+#' @examples
+#' \dontrun{
+#' # Replace the end of 10x barcode from "-1" to "-baz"
+#' bamtagregex("foo.bam", "bar.bam", "CB", "-1", "-baz")
+#' }
+bamtagregex = function(input, output, tag, pattern, replace){
+    args = c(
+        file.path(find.package("phyloRNA"), "bamtagregex.py"),
+        input, output,
+        "--tag", tag,
+        "--pattern", pattern,
+        "--replace", replace
+        )
+
+    command = getOption("phyloRNA.python")
+    systemE(command, args)
+    }
---
+++ inst/bamtagregex.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Manipulate with SAM/BAM tags using regular expression"""
+import argparse
+import re
+import pysam
+
+
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(
+        description="Manipulate with SAM/BAM tags using regular expression"""
+        )
+    parser.add_argument("input", type=str, help="an input SAM/BAM file")
+    parser.add_argument("output", type=str, help="an output SAM/BAM file")
+    parser.add_argument("--tag", type=str, help="tag that will be modified", required=True)
+    parser.add_argument("--pattern", type=str, help="a regular expression", required=True)
+    parser.add_argument("--replace", type=str, help="a replacement string", required=True)
+    args = parser.parse_args()
+    return args
+
+
+def bamtagregex():
+    """Manipulate with SAM/BAM tags using regular expression"""
+    args = parse_args()
+    pattern = re.compile(args.pattern)
+
+    with pysam.AlignmentFile(args.input, "rb") as inbam, \
+         pysam.AlignmentFile(args.output, "wb", template=inbam) as outbam:
+        for read in inbam.fetch(until_eof=True):
+            read_sub_tag(args.tag, pattern, args.replace)
+            outbam.write(read)
+
+
+def read_sub_tag(read, tag, pattern, replace):
+    """Replace tag value according to the pattern."""
+    if read.has_tag(tag):
+        value = read.get_tag(tag)
+        value = pattern.sub(value, replace)
+        read.set_tag(tag, value)
+
+
+if __name__ == "__main__":
+    bamtagregex()
