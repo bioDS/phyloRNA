726355736f3670323637c935583d95a82bec9c3e
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-09-09 13:50:43 +1200

    Added tool to filter VCF files

---
+++ R/vcftools.r
@@ -0,0 +1,24 @@
+#' Filter VCF file
+#'
+#' Filter a VCF file using the external `vcftools` software, removing indels and keeping
+#' keeping only those SNVs that passed all filters.
+#'
+#' @param input an input VCF file
+#' @param output an output VCF file
+#' @template remake
+#' @export
+vcftools_filter = function(input, output, remake=FALSE){
+    if(!remake && file.exists(output))
+        return(invisible())
+
+    args = c(
+        "--gzvcf", input,
+        "--remove-filtered-all",
+        "--remove-indels",
+        "--recode",
+        "--stdout", ">", output
+        )
+
+    command = getOptions("phyloRNA.vcftools")
+    systemE(command=command, args=args)
+    }
