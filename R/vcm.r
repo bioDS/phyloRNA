d2dfd8cf0578473bfc40d2d0f6c078fafd4acd4d
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-10-06 11:45:38 +1300

    Updated documentation

--- R/vcm.r
+++ R/vcm.r
@@ -1,10 +1,20 @@
 #' Make a variant call matrix
 #'
-#' Create a Variant Call Matrix file (VCM) by a taking the most common base for each barcode
-#' according to reads in bam file and variants in a vcf file.
+#' Create a Variant Call Matrix (vcm) by retaining the most frequent base of
+#' reads for each cell contained in a bam file and at the position of detected variants
+#' contained in the vcf file.
 #'
-#' This code is a thin wrapper over the Python script `vfm` found in the package's `inst`
+#' This code is a thin wrapper over the Python script `vcm` found in the package's `inst`
 #' folder.
+#' 
+#' This script reads a vcf file and then distribute the variants among the processes.
+#' The varchunk, chunksize, adaptive and factor are parameters that govern how will
+#' this distribution take place (see description of these parameters for more detail).
+#' As scRNAseq are notoriously patchy, variants can take highly varied time dependent
+#' on a number of reads mapped to that particular place. For this reason, the default
+#' values should provide the best performance. However, there might be situations
+#' where their modification would be beneficial.
+#'
 #'
 #' @template remake
 #' @template nthreads
@@ -16,40 +26,45 @@
 #' is used.
 #' @param min_coverage **optional** a minimum coverage for a position to not be considered
 #' unknown data
-#' @param varchunk **optional** a number of variants processed each iteration. The larger this
-#' number is, the more variants are processed at once and distributed among processes. This should
-#' reduce multiprocessing overhead and guarantee that there is enough data to send around as not
-#' all variants might pass the filter. However, this also means that more information have to be
-#' hold in memory, and read and written to a file at once.
-#' @param chunksize **optional** a number of variants assigned to a process during each iteration.
-#' Larger number reduce multiprocessing overhead but makes scheduling harder.
+#' @param varchunk **optional** By default, the whole vcf file is processed at once.
+#' Alternatively, this parameter can be specified to iterate over the vcf file and read
+#' and process only a limited number of variants at once. This shouldl improve the performance
+#' with a huge amount of cells as it decrease the size of the text that is being written
+#' in a file. However, this can also decrease performance if a small number of variants
+#' take a long time to process as other processes would wait for these.
+#' @param chunksize **optional** Number of variants assigned to a process at once. The variants
+#' can be divided into equally sized chunks that are then distributed among processes. Ideally,
+#' if the time to process each variant is the same, a larger chunks should be assigned as this
+#' reduce multiprocessing overhead. As this is not guranteed for the scRNAseq, only a single
+#' variant is send to a process at once by default.
 #' @param adaptive **optional** Use an adaptive chunksize calculation instead of a fixed number.
 #' The adaptive approach calculates chunksize for each subset of variants that passed the filter.
 #' This guarantee that the data are aways divided among the processes equally.
 #' @param factor **optional** If an adaptive chunksize is chosen, the variants that passed
 #' the filter are divided into a factor\*nthreads of equally sized subsets. Larger factor
 #' trades a smaller overhead for a potential scheduling problems.
-#' @param message **optional** Print a progress mesage.
+#' @param message **optional** Print a progress message.
 #' @export
-vfm = function(
+vcm = function(
     bam, vcf, barcodes,
     output=NULL, min_coverage=0,
-    nthreads=16, varchunk=1000, chunksize=1,
-    adaptive=FALSE, factor=4,
-    remake=FALSE, message=FALSE){
+    nthreads=16, remake=FALSE, message=FALSE,
+     varchunk=NULL, chunksize=NULL,
+    adaptive=FALSE, factor=NULL){
     args = c(
         file.path(find.package("phyloRNA"), "vcm.py"),
         bam, vcf, barcodes,
         "--min_coverage", min_coverage,
         "--nthreads", nthreads,
-        "--varchunk", varchunk,
-        "--chunksize", chunksize,
         "--factor", factor
         )
 
     if(!is_nn(output)) args = c(args, "--output", output)
+    if(!is_nn(varchunk)) args = c(args, "--varchunk", varchunk)
+    if(!is_nn(chunksize)) args = c(args, "--chunksize", chunksize)
+    if(!is_nn(factor)) args = c(args, "--factor", factor)
     if(adaptive) args = c(args, "--adaptive")
-    if(remake) args = c(args, "--remale")
+    if(remake) args = c(args, "--remake")
     if(message) args = c(args, "--message")
 
     command = getOption("phyloRNA.python")
