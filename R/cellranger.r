643a11f6cd8bc59764e096039c930ddb730abcc2
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-05-19 14:49:42 +1200

    psate0 -> paste0

--- R/cellranger.r
+++ R/cellranger.r
@@ -73,7 +73,7 @@ cellranger_count = function(fastqdir, refdir, outdir, chemistry="auto", nthreads
         paste0("--chemistry=", chemistry)
         )
     if(!is.null(sample))
-        c(args, psate0("--sample=", sample))
+        c(args, paste0("--sample=", sample))
 
     command = getOption("phyloRNA.cellranger")
     systemE(command, args, call=TRUE, dir=abspath(dirname(outdir)))
