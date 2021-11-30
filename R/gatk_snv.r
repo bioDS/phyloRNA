cb6e34cddde89b051997b4f4f322a29ba43bea6f
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-12-01 09:39:44 +1300

    bam is a vector so need to build bam index for all items

--- R/gatk_snv.r
+++ R/gatk_snv.r
@@ -49,7 +49,7 @@ gatk_snv = function(
         normal = sapply(normal, gatk_GetSampleName)
         }
 
-    gatk_BuildBamIndex(bam, remake)
+    Map(gatk_BuildBamIndex, bam, remake)
     gatk_Mutect2(bam, reference, vcf_called, normal, pon, germline, ps, remake)
     gatk_FilterMutectCalls(vcf_called, reference, vcf_filtered, remake)
     vcftools_filter(vcf_filtered, vcf_snv, remake)
